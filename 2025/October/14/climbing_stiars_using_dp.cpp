7df2f9f69f64e73732cb40a7404522e70ea91013
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-14 23:46:51 +0530

    14_10_25

---
+++ 2025/October/14/climbing_stiars_using_dp.cpp
@@ -0,0 +1,103 @@
+/*
+    author : suthakar_09
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
+#include <bits/stdc++.h>
+
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void print_1d_array(vector<int>&arr){
+    int n = arr.size();
+    // cout<<"[";
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void input_1d_array(vector<int>&arr,int n){
+    cin>>n;
+    arr.resize(n);
+    for(int i = 0 ;i<n; i++){
+        cin>>arr[i];
+    }
+}
+void input_1d_array_leetcode(vector<int>&arr,int n){
+    cin>>n;
+    string s;
+    cin >> s;
+
+    int num = 0;
+    bool inNumber = false, isNegative = false;
+
+    for (char c : s) {
+        if (c == '-') {
+            isNegative = true;
+        } 
+        else if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+            inNumber = true;
+        } 
+        else {
+            if (inNumber) {
+                if (isNegative) num = -num;
+                arr.push_back(num);
+                num = 0;
+                inNumber = false;
+                isNegative = false;
+            }
+        }
+    }
+}
+/*
+    Happy Coding
+*/
+void solve() {
+    int n;
+    cin>>n;
+    // 1 = 1
+    // 2 = 2
+    int prev1 = 1;
+    int prev2 = 2;
+    int ans = 0;
+    if(n ==1 ){
+        cout<<"1"<<endl;
+        return;
+    }
+    if(n ==2 ){
+        cout<<"2"<<endl;
+        return;
+    }
+    for(int i = 3;i<n;i++){
+        ans = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = ans;
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    
+    int a = 1;
+    cin>>a;
+    while(a--) {
+        solve();
+    }
+    return 0;
+}
---
+++ 2025/October/14/frog_jump_problem.cpp
@@ -0,0 +1,107 @@
+/*
+    author : suthakar_09
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
+#include <bits/stdc++.h>
+
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void print_1d_array(vector<int>&arr){
+    int n = arr.size();
+    // cout<<"[";
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void input_1d_array(vector<int>&arr,int &n){
+    cin>>n;
+    arr.resize(n);
+    for(int i = 0 ;i<n; i++){
+        cin>>arr[i];
+    }
+}
+void input_1d_array_leetcode(vector<int>&arr,int n){
+    cin>>n;
+    string s;
+    cin >> s;
+
+    int num = 0;
+    bool inNumber = false, isNegative = false;
+
+    for (char c : s) {
+        if (c == '-') {
+            isNegative = true;
+        } 
+        else if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+            inNumber = true;
+        } 
+        else {
+            if (inNumber) {
+                if (isNegative) num = -num;
+                arr.push_back(num);
+                num = 0;
+                inNumber = false;
+                isNegative = false;
+            }
+        }
+    }
+}
+/*
+    Happy Coding
+*/
+
+int frog_jump(vector<int>&arr, int &n){
+    int prev = 0;
+    int prev2 = 0;
+
+    for(int i = 1;i<n;i++){
+        int oneStep = prev + abs(arr[i]-arr[i-1]);
+        int twoStep = INT_MAX;
+
+        if(i > 1){
+            twoStep = prev2 + abs(arr[i] - arr[i-2]);
+        }
+
+        int curr = min(oneStep,twoStep);
+        prev2 = prev;
+        prev = curr;
+    }
+
+    return prev;
+}
+void solve() {
+    vector<int>arr;
+    int n;
+    input_1d_array(arr,n);
+    int ans = frog_jump(arr,n);
+    cout<<ans<<endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    
+    int a = 1;
+    cin>>a;
+    while(a--) {
+        solve();
+    }
+    return 0;
+} 
